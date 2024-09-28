7f6f081ae5f8a86a43c283e7abd0c2ab91c3d447
Dylan Yesid Masmela Sanchez <dylax01@gmail.com>
2024-09-27 20:18:39 -0500

    chore(comments) comments lines

--- app/src/main/java/org/masumera/service/QueryApiClient.java
+++ app/src/main/java/org/masumera/service/QueryApiClient.java
@@ -1,9 +1,9 @@
 package org.masumera.service;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+//import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+//import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
